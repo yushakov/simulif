5bdc273fbcf0483b0eb0a8fdd72e21b7daf194af
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2015-04-03 21:52:56 +0900

    Added license headers to the "simulif.exe" source code.

--- simulif/sim_system.c
+++ simulif/sim_system.c
@@ -1,9 +1,29 @@
-/*
-* sim_system.c
-*
-*  Created on: March 28, 2015
-*      Author: Yury Ushakov (yushakov.com)
-*/
+/****************************************************************************
+*  sim_system.c                                                             *
+*                                                                           *
+*  Created on:   March 28, 2015                                             *
+*  Author:       Yury Ushakov                                               *
+*  Email:        nomah@list.ru                                              *
+*                                                                           *
+*                                                                           *
+*  Copyright (C) 2015 by Yury Ushakov                                       *
+*                                                                           *
+*  This file is the part of the program "simulif".                          *
+*                                                                           *
+*  "simulif" is free software: you can redistribute it and/or modify        *
+*  it under the terms of the GNU General Public License as published by     *
+*  the Free Software Foundation, either version 3 of the License, or        *
+*  (at your option) any later version.                                      *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+*  GNU General Public License for more details.                             *
+*                                                                           *
+*  You should have received a copy of the GNU General Public License        *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                           *
+****************************************************************************/
 
 #include "sim_system.h"
 #include "rk4.h"
--- simulif/sys_parser.c
+++ simulif/sys_parser.c
@@ -1,13 +1,34 @@
-/*
-* sys_parser.c
-*
-*  Created on: March 28, 2015
-*      Author: Yury Ushakov (yushakov.com)
-*/
+/****************************************************************************
+*  sys_parser.c                                                             *
+*                                                                           *
+*  Created on:   March 28, 2015                                             *
+*  Author:       Yury Ushakov                                               *
+*  Email:        nomah@list.ru                                              *
+*                                                                           *
+*                                                                           *
+*  Copyright (C) 2015 by Yury Ushakov                                       *
+*                                                                           *
+*  This file is the part of the program "simulif".                          *
+*                                                                           *
+*  "simulif" is free software: you can redistribute it and/or modify        *
+*  it under the terms of the GNU General Public License as published by     *
+*  the Free Software Foundation, either version 3 of the License, or        *
+*  (at your option) any later version.                                      *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+*  GNU General Public License for more details.                             *
+*                                                                           *
+*  You should have received a copy of the GNU General Public License        *
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                           *
+****************************************************************************/
 #include "sys_parser.h"
 
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifndef NULL
   #define NULL 0
