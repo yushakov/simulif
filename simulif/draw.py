1c1fd1edef65b0f5ac0227deddfd0c3972ddad4e
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2015-05-31 16:44:42 +0900

    Small correction for Python 3.

--- simulif/draw.py
+++ simulif/draw.py
@@ -5,7 +5,7 @@ x = loadtxt(sys.argv[1], delimiter=",", unpack=False)
 
 cols = len(x[0])
 
-for col in xrange(cols-1):
+for col in range(cols-1):
     subplot(cols-1, 1, col+1)
     plot(x[:,0], x[:,col+1])
     if col == 0:
