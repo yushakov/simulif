3c9d301f252169b8fc2a7d5884b2a7cace270739
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2015-06-07 19:01:55 +0900

    Implemented entropy calculation. Made work more convenient (see README.md).

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # simulif
-LIF simulation
+Simulation of Leaky Integrate-and-Fire neurons network.
+Do configuration in a launching script for simulif.exe. Get trajectories at the "output.txt" file.
+
+# Processing
+1. Modify setup.py (from py_scripts) so that to reflect correct path to the "py_script" folder.
+2. Copy setup.py to some place, where you want to process your "output.txt".
+3. From the console, enter the folder containing "setup.py".
+4. From the Python console (hereafter, ">>>" means the Python's console execution):
+   >>> from setup import *
+
+5. Draw ISIDs and save ISI series:
+   >>> opu.draw_isids("folder1/output.txt", [3, 5, 7], "folder2/isiN")
+   (be careful, it will not ask for permission of "isiN*.txt" overwriting :)
+
+6. Get entropies and plots for their convergence:
+   >>> get_entropy("folder2/isiN5.txt")
--- simulif/py_scripts/setup.py
+++ simulif/py_scripts/setup.py
@@ -1,33 +1,16 @@
+# -*- coding: utf-8 -*-
 """**************************************************************************
-*  draw_distr.py                                                            *
+*  setup.py                                                                 *
 *                                                                           *
-*  Created on:   March 28, 2015                                             *
+*  Created on:   June 07, 2015                                              *
 *  Author:       Yury Ushakov                                               *
 *  Email:        nomah@list.ru                                              *
 *                                                                           *
 *                                                                           *
 *  Copyright (C) 2015 by Yury Ushakov                                       *
 *                                                                           *
-*  This script can be used to process output of the program "simulif".      *
-   Use it as following:
-
-     python draw_distr.py output_filename column_number
-
-   where "output_filename" is a file of the following format:
-
-    # comment 1
-    # comment 2
-     0.1001000,      0.1028085,      0.1040700,     -0.0017319
-     0.2002000,      0.1986785,      0.2102594,     -0.0028304
-     0.3003000,      0.2816654,      0.3045088,     -0.0067139
-     0.4004000,      0.3526658,      0.3946321,     -0.0104221
-     0.5005000,      0.4148603,      0.4644718,     -0.0104490
-     0.6006000,      0.4776937,      0.5176279,     -0.0078734
-     0.7007000,      0.5265563,      0.5679808,     -0.0104112
-     ...
-
-   and "column_number" is the number of the existing column.
-
+*  This script contains routines for processing output of                   *
+*  the program "simulif".                                                   *
 *                                                                           *
 *  This script is free software: you can redistribute it and/or modify      *
 *  it under the terms of the GNU General Public License as published by     *
@@ -43,20 +26,10 @@
 *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
 *                                                                           *
 **************************************************************************"""
-from pylab import trapz, plot, title, xlabel, ylabel, show
+
 import sys
-from out_proc_util import get_isi_distrib
+sys.path.insert(0, r"C:\Users\user1\My Documents\GitHub\simulif\simulif\py_scripts")
 
-if len(sys.argv) >= 3:
-    filename = sys.argv[1]
-    colnum   = int(sys.argv[2])
-    xvalues, distribution = get_isi_distrib(filename, colnum)
-
-    S = trapz(distribution, x=xvalues)
-    print("Area under the curve: {}".format(S))
-
-    plot(xvalues, distribution)
-    title("Interspike interval density")
-    xlabel("ISI length")
-    ylabel("Probability density")
-    show()
+import pylab as pl
+from   entropy.bin_entropy import get_entropy
+import out_proc_util as opu
